6ef4a2ed63ebd08259a347213cca96e14b30e87f
Kyle Talmage <ktalmage85@gmail.com>
2020-02-10 17:24:05 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello(name = "Programmer")
+def say_hello(name = "Ruby Programmer")
   puts "Hello Ruby #{name}!"
 end
-puts say_hello("Kyle")
+puts say_hello
